4c548acf7238da585332b1628777eed45d00d217
fmemeteau <f.memeteau@outlook.fr>
2024-08-20 11:23:52 +0200

    Ajout libvirt_kvm

---
+++ libvirt_kvm/variables.tf
@@ -0,0 +1,28 @@
+variable "img_source" {
+    description = "ISO utilisée"
+    default = "/home/fabian/ISOs/ubuntu-24.04-desktop-amd64.iso"
+}
+
+variable "img_format" {
+    description = "Format du disque utilisé"
+    default = "qcow2"
+    type = string
+}
+
+variable "mem_size" {
+    description  = "RAM allouée à la VM"
+    default = "2048"
+    type = string
+}
+
+variable "vcpu_cores" {
+    description = "Nombre de vcpus alloués"
+    default = "2"
+    type = number
+}
+
+variable "disk_size" {
+    description = "Taille du disque"
+    default = "21474836480"
+    type = number
+}
